31a2dc2710c2a8f5c2d917907302b421317c17af
awaluddin-dev <awal14h@gmail.com>
2026-08-01 21:02:55 +0700

    ver:1.2 msg:add cover-letter

--- src/widgets/cover-letter/CoverLetterModal.tsx
+++ src/widgets/cover-letter/CoverLetterModal.tsx
@@ -47,7 +47,7 @@ export function CoverLetterModal() {
             exit={{ opacity: 0 }}
             className="fixed inset-0 z-[200] flex items-center justify-center p-4"
           >
-            <div
+            <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
